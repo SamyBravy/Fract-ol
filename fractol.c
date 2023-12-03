9b3bc12dbc0000f04a0f83d13228add96a2ad03f
Samuele Dell'Erba <sdell-er@c2r5p13.42firenze.it>
2023-12-03 20:10:56 +0100

    s

--- fractol.c
+++ fractol.c
@@ -1,21 +1,32 @@
 // gcc *.c -Lminilibx-linux -lmlx -lX11 -lXext -lm
 
 #include "fractol.h"
+#define WIDTH 1920
+#define HEIGHT 1080
 
-typedef struct	s_data {
+typedef struct	s_img {
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
 	int		line_length;
 	int		endian;
+}				t_img;
+
+typedef struct	s_data {
+	void	*mlx;
+	void	*mlx_win;
+	t_img	img;
 }				t_data;
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+int	my_mlx_pixel_put(t_img *img, int x, int y, int color)
 {
 	char	*dst;
+	int		temp;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
+	temp = *(unsigned int*)dst;
 	*(unsigned int*)dst = color;
+	return (temp);
 }
 
 int	create_trgb(unsigned char t, unsigned char r, unsigned char g, unsigned char b)
@@ -43,21 +54,65 @@ unsigned char	get_b(int trgb)
 	return (((unsigned char *)&trgb)[0]);
 }
 
+int	esc(int keysym, t_data *data)
+{
+	if (keysym == XK_Escape)
+	{
+		mlx_destroy_image(data->mlx, data->img.img);
+		mlx_destroy_window(data->mlx, data->mlx_win);
+		mlx_destroy_display(data->mlx);
+		free(data->mlx);
+		return (0);
+	}
+}
+
+int	change_background(t_data *data)
+{
+	int	x;
+	int	y;
+	int	color;
+
+	x = 0;
+	y = 0;
+	color = my_mlx_pixel_put(&data->img, x, y, 0x00FF0000) + 100;
+	while (x < HEIGHT)
+	{
+		while (y < WIDTH)
+		{
+			my_mlx_pixel_put(&data->img, x, y, color);
+			y++;
+		}
+		x++;
+	}
+	mlx_put_image_to_window(data->mlx, data->mlx_win, data->img.img, 0, 0);
+}
 
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
+	t_data data;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	img.img = mlx_new_image(mlx, 1920, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
-	
-	my_mlx_pixel_put(&img, 100, 100, 0x00FF0000);
-
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+	data.mlx = mlx_init();
+	if (!data.mlx)
+		return (1);
+	data.mlx_win = mlx_new_window(data.mlx, WIDTH, HEIGHT, "Hello world!");
+	if (!data.mlx_win)
+	{
+		mlx_destroy_display(data.mlx);
+		free(data.mlx);
+		return (1);
+	}
+	data.img.img = mlx_new_image(data.mlx, WIDTH, HEIGHT);
+	if (!data.img.img)
+	{
+		mlx_destroy_window(data.mlx, data.mlx_win);
+		mlx_destroy_display(data.mlx);
+		free(data.mlx);
+		return (1);
+	}
+	data.img.addr = mlx_get_data_addr(data.img.img, &data.img.bits_per_pixel,
+								&data.img.line_length, &data.img.endian);
+	mlx_loop_hook(data.mlx, change_background, &data);
+	mlx_hook(data.mlx_win, 2, 1L<<0, esc, &data);
+	mlx_loop(data.mlx);
+	return (0);
 }
--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: sdell-er <sdell-er@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 15:02:33 by sdell-er          #+#    #+#             */
-/*   Updated: 2023/12/03 15:04:06 by sdell-er         ###   ########.fr       */
+/*   Updated: 2023/12/03 18:22:26 by sdell-er         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define FRACTOL_H
 
 #include "minilibx-linux/mlx.h"
+#include <X11/keysym.h>
+#include <stdlib.h>
 #include <math.h>
 #include <stdio.h>
 #include <unistd.h>
