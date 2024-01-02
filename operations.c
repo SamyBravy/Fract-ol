6c0c807129f35f8bb5050a52301cb7be2a7c70b8
Samuele Dell'Erba <sdell-er@c1r5p11.42firenze.it>
2024-01-02 12:15:11 +0100

    uo

--- operations.c
+++ operations.c
@@ -6,7 +6,7 @@
 /*   By: sdell-er <sdell-er@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 19:00:59 by sdell-er          #+#    #+#             */
-/*   Updated: 2023/12/07 19:01:41 by sdell-er         ###   ########.fr       */
+/*   Updated: 2024/01/02 12:11:36 by sdell-er         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ t_z	div_z(t_z z1, t_z z2)
 {
 	t_z	result;
 
-	result.real = (z1.real * z2.real + z1.imaginary * z1.imaginary)
+	result.real = (z1.real * z2.real + z1.imaginary * z2.imaginary)
 		/ (z2.real * z2.real + z2.imaginary * z2.imaginary);
 	result.imaginary = (-z1.real * z2.imaginary + z1.imaginary * z2.real)
 		/ (z2.real * z2.real + z2.imaginary * z2.imaginary);
